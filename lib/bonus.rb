66c7b5dd2024542a740a704ed26414578858680b
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-07 22:21:30 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,9 +1,6 @@
 def bonus
 epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
 
-epic_tragedy[:montague][:hero][:status] = "dead"
-
-epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 puts epic_tragedy
  
@@ -28,6 +25,10 @@ puts epic_tragedy
       ]
    }
 }
+
+epic_tragedy[:montague][:hero][:status] = "dead"
+
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
 end
