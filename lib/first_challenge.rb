f257f4792e5b940f22d577652d873d4194976e57
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-07 22:02:33 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,16 @@
 def first_challenge
-   epic_tragedy = {
-  :montague => {},
-  :capulet => {}
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
 }
 end
